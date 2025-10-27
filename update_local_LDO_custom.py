f60bebe5f96a8072e5d985133cfc98caed9c4963
David Haasnoot <Daafips@gmail.com>
2025-10-27 07:49:43 +0100

    add logging if key is invalid

--- update_local_LDO_custom.py
+++ update_local_LDO_custom.py
@@ -72,7 +72,11 @@ def haal_token_op(api_key: str) -> dict:
     response = requests.post(
         url=url_auth, headers=headers, json={"tenant": 1}, auth=("__key__", api_key)
     )
-    id_token = response.json()["access"]
+    try:
+        id_token = response.json()["access"]
+    except KeyError:
+        logger.error(f"Failed to retrieve access token: {response.text}")
+        raise UserWarning(f"Failed to retrieve access token: {response.text}")
     headers = {
         "accept": "application/json",
         "Content-Type": "application/json",
